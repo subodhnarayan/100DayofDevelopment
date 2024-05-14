aaa12f32bb38b39698bb504e0e1d5012e24cdf3b
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-05-14 17:45:34 +0530

    day14js

---
+++ Day14.js/style.js
@@ -0,0 +1,40 @@
+console.log("this is subodh");
+//single line comment
+/* This is multiple line comment*/
+/*
+moldulus %
+expenation **
+increment ++
+decrement -- 
+
+*/
+
+
+// let age = 17
+// age > 18 ? console.log("adult") : console.log("not adult")
+
+
+//Qn1 practice
+
+// let num = prompt("Enter a number:")
+// if (num % 5 == 0) {
+//     console.log("The numbe is divisble by 5");
+// }
+// else {
+//     console.log("The number is not divisible by number");
+// }
+
+
+//Q2 practice
+let mark = prompt("Enter your mark");
+if (mark >= 80 && mark <= 100) {
+    console.log("A");
+}
+else if (mark >= 70 && mark <= 89)
+    console.log("B");
+else if (mark >= 60 && mark <= 69)
+    console.log("C");
+else if (mark >= 50 && mark <= 59)
+    console.log("D");
+else
+    console.log("F")

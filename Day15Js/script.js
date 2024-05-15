cd8da59f6d4f5bc60c2950d6b4f7717c62c7358d
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-05-15 23:56:53 +0530

    day15

---
+++ Day15Js/script.js
@@ -0,0 +1,22 @@
+for (let i = 1; i <= 5; i++) {
+    console.log("This is subodh");
+}
+let j = 1;
+while (j < 5) {
+    console.log("This is subodh");
+    j++;
+}
+
+let k = 1;
+do {
+    console.log("This is subodh");
+    j++;
+} while (j < 5);
+
+console.log("Subodh");
+
+let strVar = "subodh";
+for (let val of strVar) {
+    console.log(val);
+}
+

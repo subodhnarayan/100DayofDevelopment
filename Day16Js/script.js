d9be18c3c140f3fa7d57cdb5f2f0459b92112628
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-05-16 23:40:05 +0530

    day16

---
+++ Day16Js/script.js
@@ -0,0 +1,10 @@
+console.log("This is array in js")
+let heros = ["subodh", "narayan", "sah"];
+
+let mark = [23, 34, 56];
+console.log(heros)
+let sum = 0;
+for (let x of mark) {
+    sum += x;
+}
+console.log(sum)

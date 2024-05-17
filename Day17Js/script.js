d5c147e692aab12f196b556388e181686d58287c
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-05-17 22:48:05 +0530

    day17

---
+++ Day17Js/script.js
@@ -0,0 +1,21 @@
+console.log("This is subodh");
+
+let Array = [1, 2, 3, 4, 5];
+Array.push(7);
+console.log(Array);
+Array.pop();
+console.log(Array);
+Array.toString();
+console.log(Array)
+
+let Arr = [5, 6, 7, 8, 9, 10];
+Array.concat(Arr)
+console.log(Array)
+Array.unshift(5);
+console.log(Array)
+
+
+Array.shift();
+console.log(Array)
+
+console.log(Arr.slice(3, 5))

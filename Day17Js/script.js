a66b0d0b80c0caa55794346532ba18b6e3112f4b
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-05-20 23:45:47 +0530

    array work

--- Day17Js/script.js
+++ Day17Js/script.js
@@ -3,6 +3,15 @@ console.log("This is subodh");
 let Array = [1, 2, 3, 4, 5];
 Array.push(7);
 console.log(Array);
+
+let sum = 0;
+for (let i = 0; i < Array.length; i++) {
+    sum += Array[i];
+}
+console.log("Average=" + sum / Array.length)
+
+
+
 Array.pop();
 console.log(Array);
 Array.toString();
@@ -19,3 +28,14 @@ Array.shift();
 console.log(Array)
 
 console.log(Arr.slice(3, 5))
+
+
+let compnay = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"]
+console.log(compnay)
+compnay.shift()
+console.log(compnay)
+compnay.splice(2, 1, "Ola")
+console.log(compnay)
+compnay.push("Amazon")
+console.log(compnay)
+

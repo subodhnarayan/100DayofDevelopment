19379ce4c3d86d0d286de279755e55a712f1da12
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-05-22 22:40:14 +0530

    day19

--- Day19js/script.js
+++ Day19js/script.js
@@ -1,8 +1,27 @@
-let mark = [98, 89, 92, 96, 45, 67]
+//Js5 Practice
+
+// let mark = [98, 89, 92, 96, 45, 67]
 
 
-let newmar = mark.filter((val) => {
-    return val > 90;
+// let newarr = mark.filter((val) => {
+//     return val > 90;
+// })
+
+// console.log(newarr)
+
+let n = prompt("Enter number to create array")
+let arr = [];
+for (let i = 0; i < n; i++) {
+    arr[i] = i + 1;
+}
+
+console.log(arr);
+const sum = arr.reduce((prev, curr) => {
+    return prev + curr;
+    // return prev > curr ? prev : curr;  to return the greatest number in the array
+});
+
+const prod = arr.reduce((prev, curr) => {
+    return prev * curr;
 })
-
-console.log(newarr)
+console.log(sum, prod, arr)

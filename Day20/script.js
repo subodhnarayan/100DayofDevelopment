3cb6dd5b520a1e9f3e45f67f2bc021a8f5ab1516
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-05-27 19:34:24 +0530

    day20

---
+++ Day20/script.js
@@ -0,0 +1,14 @@
+// DOM:- Document Object Model
+
+//console.log("Subodh");
+
+
+//window object:global object and created by our browser
+
+//window.console.log("hello2");
+
+console.log(document.body.childNodes[1]);
+
+
+
+

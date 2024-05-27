b9b7ace5b63662d96d5ba3c152d07b73dd0d6e5c
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-05-27 23:30:58 +0530

    day20

--- Day20/script.js
+++ Day20/script.js
@@ -7,8 +7,42 @@
 
 //window.console.log("hello2");
 
-console.log(document.body.childNodes[1]);
+//console.log(document.body.childNodes[1]);
+
+let heading = document.getElementById("division");  //getting from id
+console.log(heading)
 
 
 
+let headings = document.getElementsByClassName("heading");   // getting from class
+//console.dir(headings)
+console.log(headings)
+
+
+let head = document.getElementsByTagName("p");   //getting from tagname
+console.log(head)
+
+
+let elemet = document.querySelector("p"); //first element
+console.dir(elemet)
+
+
+let elemets = document.querySelectorAll("p"); //first element
+console.dir(elemets)
+
+
+let elem = document.querySelector(".heading"); //from class
+console.dir(elem)
+
+let el = document.querySelector("#myid");
+
+
+
+
+//DOM tree
+// three node
+
+// -text node
+// -comment node
+// -element node
 

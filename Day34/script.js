3d3ee173c6cefa9feb73bb8a0f99ccf6c0971d78
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-06-04 23:05:01 +0530

    js

---
+++ Day34/script.js
@@ -0,0 +1,10 @@
+let heading = document.getElementById("p1");
+console.dir(heading);
+
+let cl = document.getElementsByClassName("cls");
+console.dir(cl);
+console.log(cl);
+
+
+//query selector can select id,class and tag
+let firele=document.querySelector("")

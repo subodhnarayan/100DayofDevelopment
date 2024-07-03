c8fa4e6ec456814b84adc91208ee35559387b47b
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-07-03 22:42:41 +0530

    New Change

--- Day44API/script.js
+++ Day44API/script.js
@@ -1,9 +1,10 @@
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-
+// const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@1/v1";
+const BASE_URL =
+    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurrency = document.querySelector(".from select");
-const currencies = document.querySelector(".to select");
+const toCurrency = document.querySelector(".to select");
 
 for (let select of dropdowns) {
     for (currCode in countryList) {
@@ -36,7 +37,7 @@ const updateFlag = (element) => {
 }
 
 
-btn.addEventListener("click", (evt) => {
+btn.addEventListener("click", async (evt) => {
     evt.preventDefault();
     let amount = document.querySelector(".amount input");
     let amtval = amount.value;
@@ -44,13 +45,17 @@ btn.addEventListener("click", (evt) => {
         amtval = 1;
         amount.value = "1";
     }
+    console.log(fromCurrency.value, toCurrency.value);
+    const fromCurrencyValue = fromCurrency.value;
+    const toCurrencyValue = toCurrency.value;
+    const URL = `${BASE_URL}/currencies/${fromCurrencyValue.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    console.log(response);
+
 });
 
-console.log(fromCurrency.value, currencies.value);
-// const URL = `${BASE_URL}/$currencies.toLowerCase()/${fromCurrency.value.toLowerCase()}.json`;
-// let response = await fetch(URL);
-// let data = await response.json();
-// let rate = json[currencies.toLowerCase()];
+
+
 
 
 // const url = fetchJSON(`/currencies/{fromCurrency}`)
